f281834198d060fa38801fe78c1b2c773ae7b9d9
Walay Shelke <145355412+Walayhs@users.noreply.github.com>
2024-07-25 01:44:25 +0530

    Update settings.py

--- github_bot/settings.py
+++ github_bot/settings.py
@@ -90,7 +90,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
