a5d6990f1f82783f808ce31c6b986e18065defde
Walay Shelke <145355412+Walayhs@users.noreply.github.com>
2024-08-15 00:32:43 +0530

    Update web_urls.py

--- repo/web_urls.py
+++ repo/web_urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import scoreboard, issues_view, pull_request_list
+from .views import scoreboard, issues_view, pull_request_list, home
 
 urlpatterns = [
-    path('', scoreboard, name='scoreboard'),
+    path('scoreboard/', scoreboard, name='scoreboard'),
+    path('', home, name='home'),
     path('issues/', issues_view, name='issues_view'),
     path('pr/', pull_request_list, name='pull_request_list'),
 ]
